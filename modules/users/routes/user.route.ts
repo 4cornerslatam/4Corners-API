51aa237a6681b4bb1a7fc05bac55974a826021db
Sharonina <shar_mdza@hotmail.com>
2023-06-05 12:52:00 -0600

    Add user route

---
+++ modules/users/routes/user.route.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response, NextFunction } from "express";
+import { createUser, getUserById, login } from "../services/user.service";
+
+const router = express.Router();
+
+router.post("/", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const body = req.body;
+    const user = await createUser(body);
+    res.status(201).json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
